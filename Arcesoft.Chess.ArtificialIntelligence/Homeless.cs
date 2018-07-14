07b1acea61e9b7f47ea6190e74f1ef00f49026ac
Jose Arce <jose.arce@mindbodyonline.com>
2018-07-13 17:27:33 -0700

    Added stuff

--- Arcesoft.Chess.ArtificialIntelligence/Homeless.cs
+++ Arcesoft.Chess.ArtificialIntelligence/Homeless.cs
@@ -1,6 +1,22 @@
-﻿using System;
+﻿using Arcesoft.Chess.Models;
+using System;
 
 namespace Arcesoft.Chess.ArtificialIntelligence
 {
+    public class BoardScoreCalculator
+    {
+        public ScoreResult Score(IReadOnlyBoard readOnlyBoard)
+        {
+            foreach (var piece in readOnlyBoard)
+            {
 
+            }
+        }
+    }
+
+    public class ScoreResult
+    {
+        public int WhiteScore { get; set; }
+        public int BlackScore { get; set; }
+    }
 }
